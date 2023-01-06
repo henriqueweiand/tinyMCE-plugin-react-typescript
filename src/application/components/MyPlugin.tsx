01a6fb9a970c5ced982bbf06cb44601e0263f5b6
henrique <henriqueweiand@gmail.com>
2023-01-06 05:00:38 -0300

    add base files to integrate with styled-components

--- src/application/components/MyPlugin.tsx
+++ src/application/components/MyPlugin.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
-import './App.css';
+// import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const MyPlugin = () => {
   return (
-      <div className="teste" id="test">
-        <div className="aqui">
-          teste2aaa
-        </div>
-          modal with reactjs<br /><br />
+      <>
+        <Title>
+          modal with reactjs
+        </Title>
 
-          <hr />
-          <input type="file" accept="image/*" id="my-image-input"></input>
-      </div>
+        <input type="file" accept="image/*" id="my-image-input"></input>
+      </>
   );
 };
 
